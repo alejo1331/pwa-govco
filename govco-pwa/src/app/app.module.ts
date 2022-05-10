ae1ef5b14a5851d9159932be516716fba85f10f2
Jorge Pacheco <jorgepachecorod@hotmail.com>
2022-05-10 15:34:25 -0500

    feat:83 #comment Creacion del sidebar para app movil

--- govco-pwa/src/app/app.module.ts
+++ govco-pwa/src/app/app.module.ts
@@ -5,6 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { SidenavModule  } from './sidenav/sidenav.module';
+
 
 @NgModule({
   declarations: [
@@ -12,13 +19,19 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
     AppRoutingModule,
+    SidenavModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
