03e94fb3a2b4afe272b34e72594b61e03dd3d56c
Jorge Pacheco <jorgepachecorod@hotmail.com>
2022-05-17 09:53:28 -0500

    Creacion de componente de bottom-navbar

--- govco-pwa/src/app/app.module.ts
+++ govco-pwa/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { InicioModule } from './inicio/inicio.module';
+import { PerfilModule } from './perfil/perfil.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,8 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    InicioModule,
+    PerfilModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
