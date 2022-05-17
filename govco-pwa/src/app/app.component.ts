a6a53dc356fb531f6514148d43de434dfdd99542
Jorge Pacheco <jorgepachecorod@hotmail.com>
2022-05-17 04:48:05 -0500

    feat:83 #comment Creacion de animacion para contenido del menu

--- govco-pwa/src/app/app.component.ts
+++ govco-pwa/src/app/app.component.ts
@@ -1,12 +1,30 @@
 import { Component, ViewChild } from '@angular/core';
-import { MatSidenav } from '@angular/material/sidenav';
+import { MatSidenav, MatSidenavContent } from '@angular/material/sidenav';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  host: {
+    '(document:click)': 'onClick($event)',
+}
 })
+
+
 export class AppComponent {
   title = 'govco-pwa';
+
   @ViewChild(MatSidenav) sidenav!: MatSidenav;
+  @ViewChild(MatSidenavContent ) sidenavcontent!: MatSidenavContent;
+
+  constructor(){
+  }
+
+  onClick() {
+    if (this.sidenav.opened){
+
+
+    }
+  }
 }
