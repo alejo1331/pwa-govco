1ef6395440e4347ef4f63519b2b0baf115470b38
Jorge Pacheco <jorgepachecorod@hotmail.com>
2022-05-25 10:25:28 -0400

    Microinteraccion con menu lateral

--- govco-pwa/src/app/app.component.ts
+++ govco-pwa/src/app/app.component.ts
@@ -1,11 +1,43 @@
 import { Component, ViewChild } from '@angular/core';
 import { MatSidenav, MatSidenavContent } from '@angular/material/sidenav';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('efectoHamburguesaX', [
+      state('hamburgesa', style({})),
+      state(
+        'linea-superior',
+        style({
+          transform: 'rotate(45deg)',
+          transformOrigin: 'left',
+          margin: '6px',
+        })
+      ),
+      state(
+        'linea-medio',
+        style({
+          opacity: 0,
+        })
+      ),
+      
+      state(
+        'linea-inferior',
+        style({
+          transform: 'rotate(-45deg)',
+          transformOrigin: 'left',
+          margin: '6px',
+        })
+      ),
+      transition('* => *', [
+        animate('0.2s'),
+      ]),
+    ]),
+  ],
   host: {
     '(document:click)': 'onClick($event)',
 }
@@ -21,10 +53,15 @@ export class AppComponent {
   constructor(){
   }
 
+  esHamburguesa = true;
   onClick() {
-    if (this.sidenav.opened){
-
-
+    
+    if (this.sidenav.opened && this.esHamburguesa==false){
+      this.esHamburguesa = false;
+      
+    }
+    else{
+      this.esHamburguesa = !this.esHamburguesa;
     }
   }
 }
