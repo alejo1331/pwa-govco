2e013b392ea1419037b67edc2a5963e2f5295fe4
Jorge Pacheco <jorgepachecorod@hotmail.com>
2022-05-27 09:07:26 +0200

    Ajuste en distribucion de componentes

--- govco-pwa/src/app/app.module.ts
+++ govco-pwa/src/app/app.module.ts
@@ -7,6 +7,13 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { InicioModule } from './inicio/inicio.module';
 import { PerfilModule } from './perfil/perfil.module';
+import { TransversalesModule } from './transversales/transversales.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { SidenavModule  } from './sidenav/sidenav.module';
 
 @NgModule({
   declarations: [
@@ -17,6 +24,7 @@ import { PerfilModule } from './perfil/perfil.module';
     AppRoutingModule,
     InicioModule,
     PerfilModule,
+    TransversalesModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
