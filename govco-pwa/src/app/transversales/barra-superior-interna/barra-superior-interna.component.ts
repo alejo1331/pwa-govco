926beb5f05d28e1d9034915738269a99e585016a
Miguel Bohorquez <miguel.bohorquez.alzate@gmail.com>
2022-06-08 19:51:08 -0500

    barra superior secciones internas

--- govco-pwa/src/app/transversales/barra-superior-interna/barra-superior-interna.component.ts
+++ govco-pwa/src/app/transversales/barra-superior-interna/barra-superior-interna.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-barra-superior-interna',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BarraSuperiorInternaComponent implements OnInit {
 
+  @Output() outEstadoMenu = new EventEmitter<boolean>();
+  estadoMenu: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onClickMenu(){
+    this.estadoMenu = this.estadoMenu? false : true;
+    this.outEstadoMenu.emit(this.estadoMenu);
+  }
 }
