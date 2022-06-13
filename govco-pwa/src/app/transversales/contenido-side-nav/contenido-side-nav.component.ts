91c441bf3dee101e4286391ea7bd7ca67e887724
Jorge Pacheco <jorgepachecorod@hotmail.com>
2022-06-13 07:30:11 -0500

    feat:42 #comment Microinteraccion de elementos de sidenav

--- govco-pwa/src/app/transversales/contenido-side-nav/contenido-side-nav.component.ts
+++ govco-pwa/src/app/transversales/contenido-side-nav/contenido-side-nav.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContenidoSideNavComponent implements OnInit {
 
+  selectedIcon = 'outlined';
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  status: boolean = false;
+  renderer: any;
+clickEvent(){
+  console.log(this.status)
+    this.status = !this.status;
+}
+
+
+cambioIconos(event: any) {
+  var element_target = event.target.offsetParent.getElementsByClassName("material-icons-outlined")[0].classList;
+  const hasClass = element_target[0] == 'material-icons-outlined'
+  if(hasClass) {
+    element_target.add('material-icons');
+    element_target.remove('material-icons-outlined');
+  } else {
+    element_target.add('material-icons-outlined');
+    element_target.add('material-icons');
+  }
+}
 }
