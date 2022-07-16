38b271dd536e1d5f91e53084c34533bce118bb77
Jorge Pacheco <jorgepachecorod@hotmail.com>
2022-07-16 01:10:16 -0500

    Creacion de componentes

---
+++ govco-pwa/src/app/buscador/components/govco-buscador-general/govco-buscador-general.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-govco-buscador-general',
+  templateUrl: './govco-buscador-general.component.html',
+  styleUrls: ['./govco-buscador-general.component.css']
+})
+export class GovcoBuscadorGeneralComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
