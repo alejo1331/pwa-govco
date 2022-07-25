48a40228c1d026d4e8d4eac53d45b6ddb6c638a0
Miguel Bohorquez <miguel.bohorquez.alzate@gmail.com>
2022-07-25 15:31:58 -0500

    ajustes enviroment

--- govco-pwa/src/environments/environment.prod.ts
+++ govco-pwa/src/environments/environment.prod.ts
@@ -2,24 +2,26 @@ export const environment = {
   production: true,
   //General
   apiCrossUrl: "https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/cross/",
+  severApiCross: "https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/cross/",
   //Home
   serverUrlHome: "https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/home",
   serverUrlHomeAdm: "https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/home",
   //Transversales
-  urlApiFooter: 'https://api-interno.www.gov.co/api/cross/',
+  urlApiFooter: 'https://api-interno.www.gov.co/api/cross',
   //Noticias o Actualidad
   clientID: "GOVCONoticias",
   apiNoticiasDetalle: "https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/noticias/",
   serverCategoriaSubcategoriaUrl: 'https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/categorias-subcategorias/',
   serverEntidades: 'https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/entidades/',
+  //Entidades
+  serverUrl: 'https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/entidades/',
+  serverIntegracion: 'https://api-interno.www.gov.co/api/integracion-sedes/IntegracionSedes/',
+  serverUrlBuscador: "https://prebuscador.www.gov.co/api/v1",
+  serverCategorias: "https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/categorias-subcategorias/",
+  //Tu opinion cuenta o Participacion
+
 
   urlApiEntidades: 'https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/entidades',
-  serverCategorias: "https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/categorias-subcategorias/",
   urlApiTramites: "https://aws-api-interno.www.gov.co/api/ficha-tramites-y-servicios",
   serverUrlNoticias: "https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/noticias/",
-  serverUrlBuscador: "https://prebuscador.www.gov.co/api/v1",
-  serverIntegracion: 'https://api-interno.www.gov.co/api/integracion-sedes/IntegracionSedes/',
-  severApiCross: "https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/cross/",
-  serverUrl: 'http://100.24.232.62:8085/api/',
-  authIssuer: 'https://qaautenticaciondigital.and.gov.co',
 };
--- govco-pwa/src/environments/environment.ts
+++ govco-pwa/src/environments/environment.ts
@@ -1,25 +1,28 @@
 export const environment = {
   production: true,
   //General
-  apiCrossUrl: "https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/cross/",
+  apiCrossUrl: "http://100.24.232.62:8091/api/",
+  severApiCross: "http://100.24.232.62:8091/api/",
   //Home
   serverUrlHome: "http://100.24.232.62:8080/api",
   serverUrlHomeAdm: "http://100.24.232.62:8080/api",
   //Transversales
-  urlApiFooter: 'https://api-interno.www.gov.co/api/cross/',
+  urlApiFooter: 'https://api-interno.www.gov.co/api/cross',
   //Noticias o Actualidad
   clientID: "GOVCONoticias",
   authIssuer: 'https://qaautenticaciondigital.and.gov.co',
   apiNoticiasDetalle: "http://100.24.232.62:8093/api/",
   serverCategoriaSubcategoriaUrl: 'http://100.24.232.62:8087/api/',
   serverEntidades: 'http://100.24.232.62:8085/api/',
+  //Entidades
+  serverUrl: 'http://100.24.232.62:8085/api/',
+  serverIntegracion: 'https://aws-api-interno.www.gov.co/api/integracion-sedes/IntegracionSedes/',
+  serverUrlBuscador: "https://prebuscador.www.gov.co/api/v1",
+  serverCategorias: "http://100.24.232.62:8087/api/", 
+  //Tu opinion cuenta o Participacion
+
 
   urlApiEntidades: 'https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/entidades',
   serverUrlNoticias: "https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/noticias/",
-  serverCategorias: '',
-  urlApiTramites: "https://aws-api-interno.www.gov.co/api/ficha-tramites-y-servicios",
-  serverUrlBuscador: "https://prebuscador.www.gov.co/api/v1",
-  serverIntegracion: 'https://api-interno.www.gov.co/api/integracion-sedes/IntegracionSedes/',
-  severApiCross: "https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/cross/",
-  serverUrl: 'http://100.24.232.62:8085/api/',
+  urlApiTramites: "https://aws-api-interno.www.gov.co/api/ficha-tramites-y-servicios"
 };
