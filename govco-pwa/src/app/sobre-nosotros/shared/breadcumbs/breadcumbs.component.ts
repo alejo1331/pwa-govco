bd133bae777520c4a8004d641da68708b2fbf175
Leidy Nataly Ocampo Castro <mylady.oc@gmail.com>
2022-08-09 19:01:47 -0500

    #comment Se solucionan incidencias funcionales #39,40,41,47,48,54

---
+++ govco-pwa/src/app/sobre-nosotros/shared/breadcumbs/breadcumbs.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcumbs',
+  templateUrl: './breadcumbs.component.html',
+  styleUrls: ['./breadcumbs.component.scss']
+})
+export class BreadcumbsComponent implements OnInit {
+
+  @Input() title: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
