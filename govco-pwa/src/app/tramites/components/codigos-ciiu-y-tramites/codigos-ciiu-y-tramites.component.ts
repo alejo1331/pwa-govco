fa2da8b7037edb454d6ab8991b9ce48304dc4e40
Leidy Nataly Ocampo Castro <mylady.oc@gmail.com>
2022-08-22 18:07:03 -0500

    #comment Se agrega encabezado interno y se ajustan iconos de los desplegables en la consulta CIIU

--- govco-pwa/src/app/tramites/components/codigos-ciiu-y-tramites/codigos-ciiu-y-tramites.component.ts
+++ govco-pwa/src/app/tramites/components/codigos-ciiu-y-tramites/codigos-ciiu-y-tramites.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BottomMenuService } from 'src/app/transversales/services/bottom-menu/bottom-menu.service';
+import { HeaderService } from 'src/app/transversales/services/header-service/header.service';
+import { SidenavService } from 'src/app/transversales/services/sidenav-service/sidenav-service.service';
 
 @Component({
   selector: 'app-codigos-ciiu-y-tramites',
@@ -12,9 +15,17 @@ export class CodigosCiiuYTramitesComponent implements OnInit {
   title: string = 'Codigo CIIU';
   title2: string = 'Consulta de trámites por actividad económica - CIIU';
 
-  constructor() { }
+  constructor(
+    protected servicioHeader: HeaderService,
+    public bottomService: BottomMenuService,
+    protected servicioSideNav: SidenavService
+    ) { }
 
   ngOnInit(): void {
+    this.servicioHeader.estadoHeader(true, true);
+    this.bottomService.seleccionandoItem(3);
+    this.bottomService.ajustandoPantalla(false);
+    this.servicioSideNav.seleccionandoItem(false,'null');
   }
 
 }
