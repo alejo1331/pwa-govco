22ecca04fc9e3f6e396d2486c452c55d78b1baec
Leidy Nataly Ocampo Castro <mylady.oc@gmail.com>
2022-08-31 10:04:18 -0500

    #comment Se separa los cambios del environment de la solucion de incidencias sp3

--- govco-pwa/src/environments/environment.prod.ts
+++ govco-pwa/src/environments/environment.prod.ts
@@ -1,60 +1,60 @@
 export const environment = {
   production: true,
   //General
-  serverEntidades: 'https://api-interno.www.gov.co/api/entidades/',
-  serverUrl: 'https://api-interno.www.gov.co/api/entidades/',
-  serverUrlNoticia: 'https://api-interno.www.gov.co/api/noticias/',
-  urlApiEntidades: 'https://api-interno.www.gov.co/api/entidades',
-  apiUrl: "https://api-interno.www.gov.co/api/participacion/",
-  apiUrlUrna: "https://api-interno.www.gov.co/api/participacion/v2/",
+  serverEntidades: 'https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/entidades/',
+  serverUrl: 'https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/entidades/',
+  serverUrlNoticia: 'https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/noticias/',
+  urlApiEntidades: 'https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/entidades',
+  apiUrl: "https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/participacion/",
+  apiUrlUrna: "https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/participacion/v2/",
 
-  apiCrossUrl: "https://api-interno.www.gov.co/api/cross/",
-  severApiCross: "https://api-interno.www.gov.co/api/cross/",
+  apiCrossUrl: "https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/cross/",
+  severApiCross: "https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/cross/",
   //Home
-  serverUrlHome: "https://api-interno.www.gov.co/api/home",
-  serverUrlHomeAdm: "https://api-interno.www.gov.co/api/home",
-  serverUrlHomeAdmP: "https://api-interno.www.gov.co/api/home",
+  serverUrlHome: "https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/home",
+  serverUrlHomeAdm: "https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/home",
+  serverUrlHomeAdmP: "https://preapi-interno.www.gov.co/api/home",
   //Nosotros
-  serverUrlHomeSNosotros: "https://api-interno.www.gov.co/api/home",
+  serverUrlHomeSNosotros: "https://preapi-interno.www.gov.co/api/home",
   //Transversales
   urlApiFooter: 'https://api-interno.www.gov.co/api/cross',
   //Noticias o Actualidad
-  apiNoticiasDetalle: "https://api-interno.www.gov.co/api/noticias/",
-  serverCategoriaSubcategoriaUrl: 'https://api-interno.www.gov.co/api/categorias-subcategorias/',
+  apiNoticiasDetalle: "https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/noticias/",
+  serverCategoriaSubcategoriaUrl: 'https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/categorias-subcategorias/',
   codNoticias: 'actualidadprincipal',
   codParticipa: 'tuopinioncuenta',
   codSobreNosotros: 'sobrenosotros',
   codEntidades: 'paraentidades',
   //Entidades
-  serverIntegracion: 'https://aws-api-interno.www.gov.co/api/integracion-sedes/IntegracionSedes/entidad/',
+  serverIntegracion: 'https://api-interno.www.gov.co/api/integracion-sedes/IntegracionSedes/',
   //Tu opinion cuenta o Participacion
-  apiCross: "https://api-interno.www.gov.co/api/cross/",
-  apiUtils: "https://preapi-interno.www.gov.co/api/utils/",
+  apiCross: "https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/cross/",
+  apiUtils: "https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/utils/",
 
   //Biblioteca
   serverBiblioteca:"https://api-interno.www.gov.co/api/biblioteca/",
   serverUrlWebApi:"https://api-interno.www.gov.co/api/biblioteca/",
 
 
-  urlApiTramites: "https://api-interno.www.gov.co/api/ficha-tramites-y-servicios",
-  serverUrlNoticias: "https://api-interno.www.gov.co/api/noticias/",
+  urlApiTramites: "https://aws-api-interno.www.gov.co/api/ficha-tramites-y-servicios",
+  serverUrlNoticias: "https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/noticias/",
   //Categorias-Subcategorias o momentos de vida
-  serverCategorias: "https://api-interno.www.gov.co/api/categorias-subcategorias/",
+  serverCategorias: "https://preapi-interno.www.gov.co/api/categorias-subcategorias/",
 
   //URL plan de integracion
   urlPlanIntegracion: "https://admin.www.gov.co/Autenticacion/Autenticacion/RegistroAutoridad",
 
-  UrlAPIConsultaCIIU: 'https://api-interno.www.gov.co/api/ficha-tramites-y-servicios/ConsultaCIIU/',
-  auditoriaurl: 'https://api-interno.www.gov.co/api/auditoria/Auditoria/',
+  UrlAPIConsultaCIIU: 'https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/ficha-tramites-y-servicios/ConsultaCIIU/',
+  auditoriaurl: 'https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/auditoria/Auditoria/',
   serverValidateUrl: 'https://api-interno.www.gov.co/api/utils/validateurl',
 
   //Ficha tramite
-  serverUrlFichaTramite: 'https://api-interno.www.gov.co/api/ficha-tramites-y-servicios/',
+  serverUrlFichaTramite: 'https://aws-api-interno.www.gov.co/api/ficha-tramites-y-servicios/',
   //Auth
-  authIssuer: 'https://d2j6v7nsh41twk.cloudfront.net',
-  clientID: "govcomovil1QA",
+  authIssuer: 'https://qaautenticaciondigital.and.gov.co',
+  clientID: "govcomovilQA",
   authWebApi:"https://api-interno.www.gov.co/api/roles-y-permisos/RolesPerfiles/Usuarios",
-  URLgovco : 'https://d2j6v7nsh41twk.cloudfront.net/',
+  URLgovco : 'https://m.www.gov.co/',
 
   //captcha
   keyCaptcha: "6LeCqOcUAAAAAMu9McR1zoUH0z6wDj_dekAODW5h",
