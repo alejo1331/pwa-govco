84ead10d9a7bf7ff2ced90d3398a86d95fe101b3
Leidy Nataly Ocampo Castro <mylady.oc@gmail.com>
2022-09-14 18:58:24 -0500

    #comment Se continua maquetacion del acordeon

--- govco-pwa/src/app/biblioteca-pwa/components/acordeon/acordeon.component.ts
+++ govco-pwa/src/app/biblioteca-pwa/components/acordeon/acordeon.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AcordeonComponent implements OnInit {
 
+  public items = [
+    {
+      active: false,
+      titulo: 'Título #1 del acordeón',
+      descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam gravida.'
+    },
+    {
+      active: false,
+      titulo: 'Título #2 del acordeón',
+      descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam gravida.'
+    },
+    {
+      active: false,
+      titulo: 'Título #3 del acordeón',
+      descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam gravida.'
+    }      
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  activarItem(index:number) {
+    this.items.forEach(function(item){
+      item.active = false;
+    });
+    this.items[index].active = !this.items[index].active;
   }
 
 }
