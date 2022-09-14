7088c77a649067b5122fb8a52bced108dca9f46c
Julio Bermudez <julenbero@gmail.com>
2022-09-14 15:52:17 -0500

    feat: Consumo api - Banner Principal

--- govco-pwa/src/app/home-pwa/components/banner-principal-pwa/banner-principal-pwa.component.ts
+++ govco-pwa/src/app/home-pwa/components/banner-principal-pwa/banner-principal-pwa.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeService } from '../../services/home.service';
+import { BannerPrincipalModel } from '../../models/banner-principal.model';
 
 @Component({
   selector: 'app-banner-principal-pwa',
@@ -9,11 +11,31 @@ export class BannerPrincipalPwaComponent implements OnInit {
   public titleBanner: string = 'Bienvenido a GOV.CO';
   public textoBanner: string = '!No mas vueltas! Encuentra tramites, servicios e informacion del Estado colombiano'
   public textoPrimario: string = 'Facilitamos tu busqueda.'
-  public textoSecundario: string = 'Encuentra tramites y servicios segun tu momento de vida.'
+  public textoSecundario: string = ''
+  public textoBoton: string = ''
+  public textoPartido2: string = 'Facilitamos tu búsqueda. Encuentra trámites y servicios según tu momento de vida.'
+  public imagen: string = '';
+  public descripcionImagen: string = '';
 
-  constructor() { }
+  constructor(private homeService : HomeService) { }
 
-  ngOnInit() {
+  ngOnInit (): void {
+    this.homeService.getbannerPrincipal().subscribe( ( info: BannerPrincipalModel ) => {
+      if ( info['succeeded'] ) {
+        const { textoBienvenida, textoBuscador, textoAuxiliar, textoBotonAuxiliar, listaImagenes } = info['data'];
+        const textoIndex = textoAuxiliar.indexOf( '.' );
+        const textoLength = textoAuxiliar.length;
+        this.titleBanner = textoBienvenida;
+        this.textoBanner = textoBuscador;
+        this.textoPrimario = textoAuxiliar.slice(0, textoIndex + 1);
+        this.textoSecundario = textoAuxiliar.slice(textoIndex + 1, textoLength);
+        this.textoBoton = textoBotonAuxiliar;
+        const imagenAleatoria = listaImagenes[Math.floor( Math.random() * listaImagenes.length )];
+        const { urlImagen, textoDescriptivo } = imagenAleatoria;
+        this.imagen = urlImagen;
+        this.descripcionImagen = textoDescriptivo;
+      }
+    })
   }
 
 }
