6f0330ad08da10094665a8874e0d292808c6ea4c
Julio Bermudez <julenbero@gmail.com>
2022-09-28 12:05:28 -0500

    feat: Maquetacion Ficha Especifica - Ficha Tramite V1

---
+++ govco-pwa/src/app/tramites-pwa/components/ficha-especifica-pwa/ficha-especifica-pwa.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ficha-especifica-pwa',
+  templateUrl: './ficha-especifica-pwa.component.html',
+  styleUrls: ['./ficha-especifica-pwa.component.scss'],
+})
+export class FichaEspecificaPwaComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- govco-pwa/src/app/tramites-pwa/tramites-pwa.module.ts
+++ govco-pwa/src/app/tramites-pwa/tramites-pwa.module.ts
@@ -4,19 +4,13 @@ import { CommonModule } from '@angular/common';
 import { TramitesPwaRoutingModule } from './tramites-pwa-routing.module';
 import { TramitesIdComponent } from './components/tramites-principal/tramites-id.component';
 import { BibliotecaPwaModule } from '../biblioteca-pwa/biblioteca-pwa.module';
-
+import { FichaEspecificaPwaComponent } from './components/ficha-especifica-pwa/ficha-especifica-pwa.component';
+import { FichaEspecificaHeaderPwaComponent } from './components/ficha-especifica-pwa/ficha-especifica-header-pwa/ficha-especifica-header-pwa.component';
+import { FichaEspecificaDetallePwaComponent } from './components/ficha-especifica-pwa/ficha-especifica-detalle-pwa/ficha-especifica-detalle-pwa.component';
 
 @NgModule({
-  declarations: [
-    TramitesIdComponent
-  ],
-  imports: [
-    CommonModule,
-    TramitesPwaRoutingModule,
-    BibliotecaPwaModule
-  ],
-  exports: [
-
-  ]
+  declarations: [TramitesIdComponent, FichaEspecificaPwaComponent, FichaEspecificaHeaderPwaComponent, FichaEspecificaDetallePwaComponent],
+  imports: [CommonModule, TramitesPwaRoutingModule, BibliotecaPwaModule],
+  exports: [],
 })
-export class TramitesPwaModule { }
+export class TramitesPwaModule {}
