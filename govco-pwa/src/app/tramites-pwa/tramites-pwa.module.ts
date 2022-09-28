d936aa74e10fc56390d7fa364d278195eda8ca0b
Julio Bermudez <julenbero@gmail.com>
2022-09-28 12:21:26 -0500

    feat: Integracion Boton Comentarios - Ficha Tramite V1

--- govco-pwa/src/app/tramites-pwa/tramites-pwa.module.ts
+++ govco-pwa/src/app/tramites-pwa/tramites-pwa.module.ts
@@ -7,10 +7,21 @@ import { BibliotecaPwaModule } from '../biblioteca-pwa/biblioteca-pwa.module';
 import { FichaEspecificaPwaComponent } from './components/ficha-especifica-pwa/ficha-especifica-pwa.component';
 import { FichaEspecificaHeaderPwaComponent } from './components/ficha-especifica-pwa/ficha-especifica-header-pwa/ficha-especifica-header-pwa.component';
 import { FichaEspecificaDetallePwaComponent } from './components/ficha-especifica-pwa/ficha-especifica-detalle-pwa/ficha-especifica-detalle-pwa.component';
+import { HomePwaModule } from './../home-pwa/home-pwa.module';
 
 @NgModule({
-  declarations: [TramitesIdComponent, FichaEspecificaPwaComponent, FichaEspecificaHeaderPwaComponent, FichaEspecificaDetallePwaComponent],
-  imports: [CommonModule, TramitesPwaRoutingModule, BibliotecaPwaModule],
+  declarations: [
+    TramitesIdComponent,
+    FichaEspecificaPwaComponent,
+    FichaEspecificaHeaderPwaComponent,
+    FichaEspecificaDetallePwaComponent,
+  ],
+  imports: [
+    CommonModule,
+    TramitesPwaRoutingModule,
+    BibliotecaPwaModule,
+    HomePwaModule,
+  ],
   exports: [],
 })
 export class TramitesPwaModule {}
