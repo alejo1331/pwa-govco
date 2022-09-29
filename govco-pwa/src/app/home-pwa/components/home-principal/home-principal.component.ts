5e10b4c88adb983ece7210be63a4974bc29000ad
Miguel Bohorquez <miguel.bohorquez.alzate@gmail.com>
2022-09-29 17:42:16 -0500

    solucion incidencias

--- govco-pwa/src/app/home-pwa/components/home-principal/home-principal.component.ts
+++ govco-pwa/src/app/home-pwa/components/home-principal/home-principal.component.ts
@@ -61,14 +61,6 @@ export class HomePrincipalComponent implements OnInit {
 
     this.dataGeolocalizacion();
     this.dataFichaTramite();
-
-    var a = "Registro sanitario o renovación de medicamentos importados incluidos en normas farmacológicas colombianas"
-    // const a = "Afiliación en forma colectiva al sistema de seguridad social integral a trabajadores independientes miembros de agremiaciones, asociaciones y comunidades religiosas"
-    var c: number = 0
-    a.split(" ").forEach((element, i) => {
-      i < 7 ? (c += element.length) < 48 ? a = a.substring(0, 54) : a = a.substring(0, 48) : ''
-    });
-    console.log('contador:', c)
   }
 
   dataGeolocalizacion() {
@@ -105,7 +97,6 @@ export class HomePrincipalComponent implements OnInit {
         this.codigoMunicipio == "" || this.codigoMunicipio == "TodosLosMunicipios" ?
           this.tramitesService.getTramitesMasConsultados().subscribe((info: GeneralInterface) => {
             this.dataTramites = info.data;
-            console.log('dataTramites',info.data)
             this.inputTramitesMasConsultados();
           })
           : this.tramitesService.getTramitesMasConsultadosPorMunicipio(this.codigoMunicipio).subscribe((tramitesPorMunicipio: PorMunicipioInterface) => {
