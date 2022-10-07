fb630a3d505d9e8bdb7ba760df7dd934b9ab89c2
Jorge Pacheco <jorgepachecorod@hotmail.com>
2022-10-07 16:42:41 -0500

    #comment: Acordeon resultado obtenido ficha tramite

--- govco-pwa/src/app/tramites-pwa/components/tramites-id-acordeon/segundo-item-acordeon/segundo-item-acordeon.component.ts
+++ govco-pwa/src/app/tramites-pwa/components/tramites-id-acordeon/segundo-item-acordeon/segundo-item-acordeon.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
+import { TramitesPorIdService } from 'src/app/tramites-pwa/services/tramites-por-id-service/tramites-por-id.service';
 
 @Component({
   selector: 'app-segundo-item-acordeon',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./segundo-item-acordeon.component.css']
 })
 export class SegundoItemAcordeonComponent implements OnInit {
+  @Input() dataAcordeon: any;
+  resultado : any;
 
-  constructor() { }
+  constructor(
+    protected fichaTramiteService: TramitesPorIdService
+  ) { }
 
   ngOnInit() {
+
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.dataAcordeon.currentValue != undefined) {
+      this.dataAcordeon = changes.dataAcordeon.currentValue;
+      this.loadDataFichaResult(this.dataAcordeon)
+    }
+  }
+
+  private loadDataFichaResult(dataAcordeon:any){
+    this.fichaTramiteService.GetDataFichaResult(dataAcordeon.idTramite)
+    .subscribe(
+      (data) => { // Success
+        this.resultado = data;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
   }
 
 }
