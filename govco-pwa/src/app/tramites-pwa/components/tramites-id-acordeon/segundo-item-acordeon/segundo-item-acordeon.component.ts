3f1908b6b1023636e65120eecfce683d64c2d93c
Jorge Pacheco <jorgepachecorod@hotmail.com>
2022-10-07 16:29:59 -0500

    #comment: creacion del componente

---
+++ govco-pwa/src/app/tramites-pwa/components/tramites-id-acordeon/segundo-item-acordeon/segundo-item-acordeon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-segundo-item-acordeon',
+  templateUrl: './segundo-item-acordeon.component.html',
+  styleUrls: ['./segundo-item-acordeon.component.css']
+})
+export class SegundoItemAcordeonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
