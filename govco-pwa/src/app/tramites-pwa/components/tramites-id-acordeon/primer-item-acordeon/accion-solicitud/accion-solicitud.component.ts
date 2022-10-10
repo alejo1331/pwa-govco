9be5c813e7fbdfea4c25defdae7fec005f859ea4
Leidy Nataly Ocampo Castro <mylady.oc@gmail.com>
2022-10-10 18:50:44 -0500

    #comment Se agrega enlace puntos de atencion al tipo de accion solicitud

--- govco-pwa/src/app/tramites-pwa/components/tramites-id-acordeon/primer-item-acordeon/accion-solicitud/accion-solicitud.component.ts
+++ govco-pwa/src/app/tramites-pwa/components/tramites-id-acordeon/primer-item-acordeon/accion-solicitud/accion-solicitud.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { ValidateUrlService } from 'src/app/tramites-pwa/services/validate-url.service';
 
 @Component({
@@ -7,14 +7,44 @@ import { ValidateUrlService } from 'src/app/tramites-pwa/services/validate-url.s
   styleUrls: ['./accion-solicitud.component.scss']
 })
 export class AccionSolicitudComponent implements OnInit {
+  @ViewChild('seccionTramitesId') seccionTramitesId: ElementRef;
+  @ViewChild('seccionPuntoAtencion') seccionPuntoAtencion: ElementRef;
 
   @Input() data: any;
+  @Input() dataAcordeon: any;
+
+  activarPuntosAtecion: boolean = false;
+
+  topScroll: HTMLElement;
 
   constructor(
     public validateUrlService: ValidateUrlService
   ) { }
 
   ngOnInit(): void {
+    this.topScroll = (document.getElementById('topScroll') as HTMLElement)
+    this.topScroll.style.height = '100%';
+    this.topScroll.scrollTop = 0;
+    this.topScroll.style.top = '0';
+  }
+
+  abrirPuntosAtencion() {
+    this.activarPuntosAtecion = true;
+    const abrirPuntosAtencion: string = '0%';
+    const cerrarTramitesId: string = '-100%';
+    this.seccionTramitesId.nativeElement.style.left = cerrarTramitesId;
+    this.seccionPuntoAtencion.nativeElement.style.left = abrirPuntosAtencion;
+    this.topScroll.scrollTop = 0;
+  }
+
+  cerrarPuntosAtencion([cerrarPuntosAtencion, abrirTramitesId]: [string, string]) {
+    this.seccionTramitesId.nativeElement.style.left = abrirTramitesId;
+    this.seccionPuntoAtencion.nativeElement.style.left = cerrarPuntosAtencion;
+    this.topScroll.scrollTop = 0;
+    this.seccionTramitesId.nativeElement.addEventListener("transitionend", () => {
+      this.seccionTramitesId.nativeElement.style.left == '0%' ?
+        this.activarPuntosAtecion = false : null;
+    })
   }
 
 }
