e9ee12b87c9d47b2be22be52871af184eae3e18d
Jorge Pacheco <jorgepachecorod@hotmail.com>
2022-10-11 10:47:26 -0500

    #comment: Selector multiple de legislacion

--- govco-pwa/src/app/tramites-pwa/components/tramites-id-acordeon/quinto-item-acordeon/quinto-item-acordeon.component.ts
+++ govco-pwa/src/app/tramites-pwa/components/tramites-id-acordeon/quinto-item-acordeon/quinto-item-acordeon.component.ts
@@ -9,6 +9,8 @@ import { TramitesPorIdService } from 'src/app/tramites-pwa/services/tramites-por
 export class QuintoItemAcordeonComponent implements OnInit {
   @Input() dataAcordeon: any;
   normatividad :any = [];
+  toggleBool=true;
+  selectedItemsList : any = [];
 
   constructor(
     protected fichaTramiteService: TramitesPorIdService
@@ -37,5 +39,34 @@ export class QuintoItemAcordeonComponent implements OnInit {
         this.normatividad = n;
       });
   }
+
+  toogleSeleccion(){
+    let estadoSelectorGeneral : any = document.getElementsByName('selectorGeneral')[0];
+    let normas : any = document.getElementsByName('norma')
+    if (estadoSelectorGeneral.checked){
+      for (let i=0; i < normas.length; i++){
+        normas[i].checked=true;
+      }
+    }
+    else{
+      for (let i=0; i < normas.length; i++){
+        normas[i].checked=false;
+      }
+    }
+  }
+
+  descargarSeleccion(){
+
+    let normas : any = document.getElementsByName('norma')
+    for (let i=0; i < normas.length; i++){
+      if(normas[i].checked==true){
+        this.selectedItemsList.push(normas[i].value);
+      }
+    }
+    debugger
+    this.selectedItemsList.forEach((e : any) =>  self.open(e),"_self")
+   }
+
+
 
 }
