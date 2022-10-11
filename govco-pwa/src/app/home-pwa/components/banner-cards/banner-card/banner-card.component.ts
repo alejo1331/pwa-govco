b92867b6a1344cad9177d3b35711fcc485ec82f2
Julio Bermudez <julenbero@gmail.com>
2022-10-11 12:03:44 -0500

    fix: Limpieza de codigo - Home PWA

--- govco-pwa/src/app/home-pwa/components/banner-cards/banner-card/banner-card.component.ts
+++ govco-pwa/src/app/home-pwa/components/banner-cards/banner-card/banner-card.component.ts
@@ -1,26 +1,25 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-banner-card',
   templateUrl: './banner-card.component.html',
-  styleUrls: ['./banner-card.component.scss']
+  styleUrls: ['./banner-card.component.scss'],
 })
 export class BannerCardComponent implements OnInit {
-
   @Input() iconNameClass: string;
   @Input() textNameClass: string;
   @Input() tagName: string;
 
-  constructor(private activateRoute: ActivatedRoute) { }
+  constructor(private activateRoute: ActivatedRoute) {}
 
-  ngOnInit (): void {
-    this.activateRoute.fragment.subscribe( ( value : any ) => {
-      this.onClickTag( value );
-    })
+  ngOnInit(): void {
+    // this.activateRoute.fragment.subscribe( ( value : any ) => {
+    //   this.onClickTag( value );
+    // })
   }
 
-  onClickTag (section : any): void { 
-    document.getElementById(section)?.scrollIntoView( { behavior: 'smooth' } )
+  onClickTag(section: any) {
+    document.getElementById(section)?.scrollIntoView({ behavior: 'smooth' });
   }
 }
