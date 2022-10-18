d7d043f939cc2353b74875f6a71de38226e8b062
Julio Bermudez <julenbero@gmail.com>
2022-10-18 00:42:41 -0500

    fix: Ajustes elementos modal dudas - Ficha Tramite V1

--- govco-pwa/src/app/tramites-pwa/components/modal-dudas-pwa/modal-dudas-pwa.component.ts
+++ govco-pwa/src/app/tramites-pwa/components/modal-dudas-pwa/modal-dudas-pwa.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-modal-dudas-pwa',
@@ -8,8 +9,34 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class ModalDudasPwaComponent implements OnInit {
   @Input() canalesSeguimiento: any;
+  listaEmail: string[];
+  listaNumeros: string[];
 
   constructor(public activeModal: NgbActiveModal) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (this.canalesSeguimiento.length === 1) {
+      this.listaEmail = this.canalesSeguimiento
+        .map((data: any) => data.Correo)
+        .filter(Boolean);
+      this.listaNumeros = this.canalesSeguimiento
+        .map((data: any) => data.Telefono)
+        .filter(Boolean);
+    } else {
+      let listaEmailFilter = new Set(
+        this.canalesSeguimiento
+          .filter((data: any) => data.Canal === 'CORREO')
+          .map((data: any) => data.Correo)
+          .filter(Boolean)
+      );
+      this.listaEmail = [...listaEmailFilter] as string[];
+      let listaNumerosFilter = new Set(
+        this.canalesSeguimiento
+          .filter((data: any) => data.Canal === 'TELEFONICO')
+          .map((data: any) => data.Telefono)
+          .filter(Boolean)
+      );
+      this.listaNumeros = [...listaNumerosFilter] as string[];
+    }
+  }
 }
