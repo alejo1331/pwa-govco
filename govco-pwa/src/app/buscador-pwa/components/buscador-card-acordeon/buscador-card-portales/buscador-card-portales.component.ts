8f442b44a7e685c3274b8da30c71976a4d19ea9b
Miguel Bohorquez <miguel.bohorquez.alzate@gmail.com>
2022-11-02 15:34:46 -0500

    consumo datos para las tarjetas cards: tramites, entidades, ventanillas y portales

--- govco-pwa/src/app/buscador-pwa/components/buscador-card-acordeon/buscador-card-portales/buscador-card-portales.component.ts
+++ govco-pwa/src/app/buscador-pwa/components/buscador-card-acordeon/buscador-card-portales/buscador-card-portales.component.ts
@@ -1,18 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { PortalTransversalesInterface } from 'src/app/buscador-pwa/models/portal-transversales-interface';
+import { urlsLocal } from 'src/variables-globales/urlsLocal';
 
 @Component({
   selector: 'app-buscador-card-portales',
   templateUrl: './buscador-card-portales.component.html',
   styleUrls: ['./buscador-card-portales.component.scss'],
 })
-export class BuscadorCardPortalesComponent implements OnInit {
-  titleAcordeon = 'Portal de Datos del Estado colombiano';
-  data = {
-    Entidad: 'I.P.S. Centro de Salud - Macaravita',
-    PaginaWeb: 'www.google.com',
-    DescripcionTramite:
-      ' Es una estrategia de articulación público privada que busca mejorar el entorno para el desarrollo de la actividad. El Ministerio de Educación Nacional y el Ministerio de Ciencia, Tecnología e Innovación invitan a los Docentes.',
-  };
+export class BuscadorCardPortalesComponent implements OnInit, OnChanges {
+
+  @Input() data: PortalTransversalesInterface[];
+
+  public items: {
+    active: boolean,
+    descripcion: string;
+    entidadNombre: string;
+    link: string;
+    linkEntidad: string;
+    titulo: string;
+  }[] = [];
+  href: boolean = true;
 
   infoDescripcionTramite: any;
   contenidoDescripcion: string;
@@ -25,13 +32,38 @@ export class BuscadorCardPortalesComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    this.infoDescripcionTramite = this.data;
-    this.contenidoDescripcion =
-      this.infoDescripcionTramite.DescripcionTramite.substring(0, 125) + '...';
-    this.contenidoLeido = this.infoDescripcionTramite.DescripcionTramite;
-    if (this.contenidoDescripcion.length > 124) {
-      this.caracteresCategoria = true;
-    }
+    
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.data.previousValue != changes.data.currentValue) {
+      this.items = []
+    };
+    changes.data.currentValue.forEach((element:PortalTransversalesInterface) => {
+      this.href = true;
+      Object.values(urlsLocal).find(url => {
+         element.link.indexOf(url) >= 0 ? this.href = false : null;
+      })
+      this.items.push(
+        {
+          active: false,
+          descripcion: element.descripcion,
+          entidadNombre: element.entidadNombre,
+          link: element.link,
+          linkEntidad: element.linkEntidad,
+          titulo: element.titulo
+        }
+      )
+    })
+  }
+
+  activarItem(index: number) {
+    this.items[index].active = !this.items[index].active;
+    this.items.forEach(function (item, indexItem) {
+      if (indexItem != index) {
+        item.active = false;
+      }
+    });
   }
 
   showExpended() {
--- govco-pwa/src/app/buscador-pwa/components/buscador-card-acordeon/buscador-card-ventanilla/buscador-card-ventanilla.component.ts
+++ govco-pwa/src/app/buscador-pwa/components/buscador-card-acordeon/buscador-card-ventanilla/buscador-card-ventanilla.component.ts
@@ -1,39 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { VentanillasUnicasInterface } from 'src/app/buscador-pwa/models/ventanillas-unicas-interface';
+import { urlsLocal } from 'src/variables-globales/urlsLocal';
 
 @Component({
   selector: 'app-buscador-card-ventanilla',
   templateUrl: './buscador-card-ventanilla.component.html',
   styleUrls: ['./buscador-card-ventanilla.component.scss']
 })
-export class BuscadorCardVentanillaComponent implements OnInit {
+export class BuscadorCardVentanillaComponent implements OnInit, OnChanges {
 
-  public items = [
-    {
-      active: false,
-      titulo: 'Ventanilla Única Empresarial',
-      responsable:'Ministerio de Comercio, Industria y Turismo',
-      urlVentanillas: '/buscar-pwa',
-      descripcion: 'Es una estrategia de articulación público privada que busca mejorar el entorno para el desarrollo de la actividad.',
-    }, 
-    {
-      active: false,
-      titulo: 'Ventanilla Única Empresarial',
-      responsable:'Ministerio de Comercio, Industria y Turismo',
-      urlVentanillas: '/buscar-pwa',
-      descripcion: 'Es una estrategia de articulación público privada que busca mejorar el entorno para el desarrollo de la actividad.',
-    },
-    {
-      active: false,
-      titulo: 'Ventanilla Única Empresarial',
-      responsable:'Ministerio de Comercio, Industria y Turismo',
-      urlVentanillas: '/buscar-pwa',
-      descripcion: 'Es una estrategia de articulación público privada que busca mejorar el entorno para el desarrollo de la actividad.',
-    }      
-  ]
+  @Input() data: VentanillasUnicasInterface[];
+
+  public items: {
+    active: boolean,
+    descripcion: string;
+    entidadNombre: string;
+    link: string;
+    linkEntidad: string;
+    titulo: string;
+  }[] = [];
+  href: boolean = true;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.data.previousValue != changes.data.currentValue) {
+      this.items = []
+    };
+    changes.data.currentValue.forEach((element: VentanillasUnicasInterface) => {
+      this.href = true;
+      Object.values(urlsLocal).find(url => {
+        element.link.indexOf(url) >= 0 ? this.href = false : null;
+      })
+      this.items.push(
+        {
+          active: false,
+          descripcion: element.descripcion,
+          entidadNombre: element.entidadNombre,
+          link: element.link,
+          linkEntidad: element.linkEntidad,
+          titulo: element.titulo
+        }
+      )
+    })
   }
 
   activarItem(index: number) {
