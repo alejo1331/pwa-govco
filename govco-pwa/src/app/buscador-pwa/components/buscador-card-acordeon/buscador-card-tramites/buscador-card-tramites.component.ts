e5ee42fd14fabfc9035634e11e7056c5bfc56a26
Julio Bermudez <julenbero@gmail.com>
2022-11-03 20:38:40 -0500

    fix: Limpieza de codigo - Buscador PWA

--- govco-pwa/src/app/buscador-pwa/components/buscador-card-acordeon/buscador-card-tramites/buscador-card-tramites.component.ts
+++ govco-pwa/src/app/buscador-pwa/components/buscador-card-acordeon/buscador-card-tramites/buscador-card-tramites.component.ts
@@ -1,4 +1,14 @@
+<<<<<<< HEAD
 import { Component, Input, OnInit, OnChanges, SimpleChanges, ViewChildren, ElementRef, QueryList } from '@angular/core';
+=======
+import {
+  Component,
+  Input,
+  OnInit,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
+>>>>>>> 8ee8626 (fix: Limpieza de codigo - Buscador PWA)
 import { TramitesServiciosInterface } from 'src/app/buscador-pwa/models/tramites-servicios-interface';
 import { urlsLocal } from 'src/variables-globales/urlsLocal';
 import { ValidarUrlService } from 'src/app/buscador-pwa/services/validar-url.service';
@@ -30,25 +40,29 @@ export class BuscadorCardTramitesComponent implements OnInit, OnChanges {
   expandirTexto: boolean = false;
   botonTexto: boolean[] = [];
 
+<<<<<<< HEAD
   constructor(
     public validarUrlService: ValidarUrlService
   ) { }
+=======
+  showBotonFechas: boolean;
 
-  ngOnInit(): void {
-  }
+  constructor() {}
+>>>>>>> 8ee8626 (fix: Limpieza de codigo - Buscador PWA)
+
+  ngOnInit(): void {}
 
   ngDoCheck() {
-
-    if (this.items.length > 0){
-
-      if(document.getElementById('tramitesAcordeon')){
-        $('#tramitesAcordeon div.card:nth-child(-n+5)').addClass('actived')
+    if (this.items.length > 0) {
+      if (document.getElementById('tramitesAcordeon')) {
+        $('#tramitesAcordeon div.card:nth-child(-n+5)').addClass('actived');
       }
     }
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.data.previousValue != changes.data.currentValue) {
+<<<<<<< HEAD
       this.items = []
     };
     changes.data.currentValue.forEach((element: TramitesServiciosInterface, i: number) => {
@@ -71,6 +85,49 @@ export class BuscadorCardTramitesComponent implements OnInit, OnChanges {
         }
       )
     })
+=======
+      this.items = [];
+    }
+    changes.data.currentValue.forEach((element: TramitesServiciosInterface) => {
+      this.href = true;
+      Object.values(urlsLocal).find((url) => {
+        element.link.indexOf(url) >= 0 ? (this.href = false) : null;
+      });
+      this.items.push({
+        costo: element.costo,
+        descripcion: element.descripcion,
+        enLinea: element.enLinea,
+        entidad: element.entidad,
+        link: element.link,
+        linkEntidad: element.linkEntidad,
+        tiempoObtencion: element.tiempoObtencion,
+        titulo: element.titulo,
+      });
+    });
+  }
+
+  showExpended() {
+    if (this.nombreExpanded === 'Leer más...') {
+      this.contenidoDescripcion =
+        this.infoDescripcionTramite.DescripcionTramite;
+      this.nombreExpanded = 'Leer menos';
+    }
+    return this.contenidoDescripcion;
+  }
+
+  showBotonLeer() {
+    if (this.nombreExpanded === 'Leer más...') {
+      this.contenidoDescripcion =
+        this.infoDescripcionTramite.DescripcionTramite;
+      this.nombreExpanded = 'Leer menos';
+    } else {
+      this.nombreExpanded = 'Leer más...';
+      this.contenidoDescripcion =
+        this.infoDescripcionTramite.DescripcionTramite.substring(0, 125) +
+        '...';
+    }
+    return this.contenidoDescripcion;
+>>>>>>> 8ee8626 (fix: Limpieza de codigo - Buscador PWA)
   }
 
   activarItem(index: number) {
@@ -100,11 +157,12 @@ export class BuscadorCardTramitesComponent implements OnInit, OnChanges {
     this.listaTexto.toArray()[index].nativeElement.classList.toggle('line-clamp-3');
     this.expandirTexto = (this.expandirTexto == true) ? false : true;
   }
-  VerMasResultados(){
+  VerMasResultados() {
     let resultadosActivos = $('div.card');
     let ultimoActivo = resultadosActivos.filter('.actived:last').index();
-    resultadosActivos.filter(':lt(' + (ultimoActivo + 6) + ')').addClass('actived');
-
+    resultadosActivos
+      .filter(':lt(' + (ultimoActivo + 6) + ')')
+      .addClass('actived');
   }
   
 }
