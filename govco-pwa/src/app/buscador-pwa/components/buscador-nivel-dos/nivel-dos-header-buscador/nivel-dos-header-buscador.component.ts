4e1ed1fc1383a9e5756ae00f3c9e9edce874e998
Jorge Pacheco <jorgepachecorod@hotmail.com>
2022-11-17 08:37:56 -0500

    #comment: Solucion de incidencia busqueda con tildes

--- govco-pwa/src/app/buscador-pwa/components/buscador-nivel-dos/nivel-dos-header-buscador/nivel-dos-header-buscador.component.ts
+++ govco-pwa/src/app/buscador-pwa/components/buscador-nivel-dos/nivel-dos-header-buscador/nivel-dos-header-buscador.component.ts
@@ -33,7 +33,6 @@ export class NivelDosHeaderBuscadorComponent implements AfterViewInit {
 
   ngAfterViewInit() {
     var input = this.input.nativeElement
-    let prefiltroInicial : any = document.getElementsByClassName('filtro-active')[0]
     this.buscadorService.getBuscadorParams$.subscribe(
       (parametros: BuscadorParams) => {
         this.txtConsumoApi = parametros.txtConsumoApi;
@@ -41,12 +40,9 @@ export class NivelDosHeaderBuscadorComponent implements AfterViewInit {
         this.index = parametros.index;
         input.value = this.txtInputBuscador
         if ((input.value == parametros.txtInputBuscador) && (input.value != '')) {
-          let prefiltroActual = document.getElementsByClassName('filtro-active')[0]
-          if (prefiltroInicial == prefiltroActual && this.activarServicio === true){
-            this.cerrarBuscadorPWA();
-            this.buscar()
-          }
-          prefiltroInicial = prefiltroActual;
+         if (this.activarServicio === true){
+                this.buscar()
+        }
           this.buscarSugerencias()
         }
       }
@@ -60,11 +56,12 @@ export class NivelDosHeaderBuscadorComponent implements AfterViewInit {
       this.buscadorService.setSugerenrciasBuscador(this.datosAutocompletar)
     }
     else {
+      let inputBuscadorSinTildes = inputBuscador.value.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
       this.sugerenciasService.obtenerSugerencias(this.txtConsumoApi,
-        inputBuscador.value,
+        inputBuscadorSinTildes,
         this.numeroSugerencias).subscribe((data) => {
           this.datosAutocompletar = data.filtros[0].sugerenciasFiltro;
-          let regEx = new RegExp(inputBuscador.value, 'gi');
+          let regEx = new RegExp(inputBuscadorSinTildes, 'gi');
           this.datosAutocompletar.forEach((name: any, index: any) => {
             this.datosAutocompletar[index] = [name, name.replace(regEx, "<strong>$&</strong>")]
           });
