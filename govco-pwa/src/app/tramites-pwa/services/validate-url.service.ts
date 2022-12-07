3d5669a877af06fc0b19f38d60f49d32e1da8cc1
Jorge Pacheco <jorgepachecorod@hotmail.com>
2022-12-07 10:06:08 -0500

    #comment: Solucion incidencia #31

--- govco-pwa/src/app/tramites-pwa/services/validate-url.service.ts
+++ govco-pwa/src/app/tramites-pwa/services/validate-url.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { MatDialog } from '@angular/material/dialog';
 import { ModalErrorUrlComponent } from 'src/app/biblioteca-pwa/components/modal-error-url/modal-error-url.component';
+import { timeout } from 'rxjs/operators';
 
 const API_URL = environment.serverValidateUrl;
 
@@ -15,12 +16,16 @@ export class ValidateUrlService {
     private http: HttpClient,
     public dialog: MatDialog
   ) { }
-  
+
   private getGeneric<T>(endPoint: string, parameters: string = '') {
-    return this.http.get<T>(`${API_URL}${endPoint}${parameters}`, {headers: new HttpHeaders().append('angular-show-loading', 'true')});
+    return this.http.get<T>(`${API_URL}${endPoint}${parameters}`, {headers: new HttpHeaders().append('angular-show-loading', 'true')})
+    .pipe(
+      timeout(3000)
+    );
   }
 
   validate(url: string) {
+    console.log(url)
     return this.getGeneric<boolean>('?url=', encodeURIComponent(url));
   }
 
@@ -28,12 +33,17 @@ export class ValidateUrlService {
     this.validate(url)
     .subscribe((data: boolean) => {
         if (data) {
+
             window.open(url, "target='_blank'");
         } else {
             this.dialog.open(ModalErrorUrlComponent, {
               width: '280px'
             });
         }
+    }, error => {
+      this.dialog.open(ModalErrorUrlComponent, {
+        width: '280px'
+      });
     });
   }
 }
