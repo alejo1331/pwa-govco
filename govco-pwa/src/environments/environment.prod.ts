45bc181dee7f2287ac6a135b94a0e047125d9dd9
Julio Bermudez <julenbero@gmail.com>
2022-12-12 15:40:13 -0500

    fix: ajuste codigos env - PWA

--- govco-pwa/src/environments/environment.prod.ts
+++ govco-pwa/src/environments/environment.prod.ts
@@ -5,66 +5,78 @@ export const environment = {
   serverUrl: 'https://api-interno.www.gov.co/api/entidades/',
   serverUrlNoticia: 'https://api-interno.www.gov.co/api/noticias/',
   urlApiEntidades: 'https://api-interno.www.gov.co/api/entidades',
-  apiUrl: "https://api-interno.www.gov.co/api/participacion/",
-  apiUrlUrna: "https://api-interno.www.gov.co/api/participacion/",
+  apiUrl: 'https://api-interno.www.gov.co/api/participacion/',
+  apiUrlUrna: 'https://api-interno.www.gov.co/api/participacion/',
 
-  apiCrossUrl: "https://api-interno.www.gov.co/api/cross/",
-  severApiCross: "https://api-interno.www.gov.co/api/cross/",
+  apiCrossUrl: 'https://api-interno.www.gov.co/api/cross/',
+  severApiCross: 'https://api-interno.www.gov.co/api/cross/',
   //Home
-  serverUrlHome: "https://api-interno.www.gov.co/api/home",
-  serverUrlHomeAdm: "https://api-interno.www.gov.co/api/home",
-  serverUrlHomeAdmP: "https://api-interno.www.gov.co/api/home",
+  serverUrlHome: 'https://api-interno.www.gov.co/api/home',
+  serverUrlHomeAdm: 'https://api-interno.www.gov.co/api/home',
+  serverUrlHomeAdmP: 'https://api-interno.www.gov.co/api/home',
   //Nosotros
-  serverUrlHomeSNosotros: "https://api-interno.www.gov.co/api/home",
+  serverUrlHomeSNosotros: 'https://api-interno.www.gov.co/api/home',
   //Transversales
   urlApiFooter: 'https://api-interno.www.gov.co/api/cross',
   //Noticias o Actualidad
-  apiNoticiasDetalle: "https://api-interno.www.gov.co/api/noticias/",
-  serverCategoriaSubcategoriaUrl: 'https://api-interno.www.gov.co/api/categorias-subcategorias/',
+  apiNoticiasDetalle: 'https://api-interno.www.gov.co/api/noticias/',
+  serverCategoriaSubcategoriaUrl:
+    'https://api-interno.www.gov.co/api/categorias-subcategorias/',
   codNoticias: 'actualidadprincipal',
-  codParticipa: 'tuopinioncuenta',
+  codParticipa: 'participacion',
   codSobreNosotros: 'sobrenosotros',
-  codEntidades: 'paraentidades',
+  codEntidades: 'servicios-para-entidades',
   //Entidades
-  serverIntegracion: 'https://api-interno.www.gov.co/api/integracion-sedes/IntegracionSedes/entidad/',
+  serverIntegracion:
+    'https://api-interno.www.gov.co/api/integracion-sedes/IntegracionSedes/entidad/',
   //Tu opinion cuenta o Participacion
-  apiCross: "https://api-interno.www.gov.co/api/cross/",
-  apiUtils: "https://api-interno.www.gov.co/api/utils/",
+  apiCross: 'https://api-interno.www.gov.co/api/cross/',
+  apiUtils: 'https://api-interno.www.gov.co/api/utils/',
   //Biblioteca
-  serverBiblioteca:"https://api-interno.www.gov.co/api/biblioteca/",
-  serverUrlWebApi:"https://api-interno.www.gov.co/api/biblioteca/",
+  serverBiblioteca: 'https://api-interno.www.gov.co/api/biblioteca/',
+  serverUrlWebApi: 'https://api-interno.www.gov.co/api/biblioteca/',
 
-  urlApiTramites: "https://api-interno.www.gov.co/api/ficha-tramites-y-servicios",
-  serverUrlNoticias: "https://api-interno.www.gov.co/api/noticias/",
+  urlApiTramites:
+    'https://api-interno.www.gov.co/api/ficha-tramites-y-servicios',
+  serverUrlNoticias: 'https://api-interno.www.gov.co/api/noticias/',
   //Categorias-Subcategorias o momentos de vida
-  serverCategorias: "https://api-interno.www.gov.co/api/categorias-subcategorias/",
+  serverCategorias:
+    'https://api-interno.www.gov.co/api/categorias-subcategorias/',
 
   //URL plan de integracion
-  urlPlanIntegracion: "https://admin.www.gov.co/Autenticacion/Autenticacion/RegistroAutoridad",
+  urlPlanIntegracion:
+    'https://admin.www.gov.co/Autenticacion/Autenticacion/RegistroAutoridad',
 
-  UrlAPIConsultaCIIU: 'https://api-interno.www.gov.co/api/ficha-tramites-y-servicios/ConsultaCIIU/',
+  UrlAPIConsultaCIIU:
+    'https://api-interno.www.gov.co/api/ficha-tramites-y-servicios/ConsultaCIIU/',
   auditoriaurl: 'https://api-interno.www.gov.co/api/auditoria/Auditoria/',
   serverValidateUrl: 'https://api-interno.www.gov.co/api/utils/validateurl',
 
   //Ficha tramite
-  serverUrlFichaTramite: 'https://api-interno.www.gov.co/api/ficha-tramites-y-servicios/',
+  serverUrlFichaTramite:
+    'https://api-interno.www.gov.co/api/ficha-tramites-y-servicios/',
   //ventanilla unica
-  serverSedesElectronicas: "https://api-interno.www.gov.co/api/integracion-sedes",
+  serverSedesElectronicas:
+    'https://api-interno.www.gov.co/api/integracion-sedes',
   //Auth
   authIssuer: 'https://autenticaciondigital.and.gov.co',
-  clientID: "govcomovilPRO",
-  authWebApi:"https://api-interno.www.gov.co/api/roles-y-permisos/RolesPerfiles/Usuarios",
-  URLgovco : 'https://m.www.gov.co/',
+  clientID: 'govcomovilPRO',
+  authWebApi:
+    'https://api-interno.www.gov.co/api/roles-y-permisos/RolesPerfiles/Usuarios',
+  URLgovco: 'https://m.www.gov.co/',
   //WebComponent
-  serverFichaTramite:'https://api-interno.www.gov.co/api/ficha-tramites-y-servicios',
+  serverFichaTramite:
+    'https://api-interno.www.gov.co/api/ficha-tramites-y-servicios',
 
   //buscador
-  serverBuscador: 'https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/buscador/',
-  serverUrlValidarUrl: 'https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api',
+  serverBuscador:
+    'https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api/buscador/',
+  serverUrlValidarUrl:
+    'https://a1u3bqb22e.execute-api.us-east-1.amazonaws.com/qa/api',
 
   //captcha
-  keyCaptcha: "6LeCqOcUAAAAAMu9McR1zoUH0z6wDj_dekAODW5h",
+  keyCaptcha: '6LeCqOcUAAAAAMu9McR1zoUH0z6wDj_dekAODW5h',
 
   //Carpeta ciudadana
-  carpetaciudadana:"https://carpetaciudadana.and.gov.co/",
+  carpetaciudadana: 'https://carpetaciudadana.and.gov.co/',
 };
--- govco-pwa/src/environments/environment.ts
+++ govco-pwa/src/environments/environment.ts
@@ -5,66 +5,76 @@ export const environment = {
   serverUrl: 'https://preapi-interno.www.gov.co/api/entidades/',
   serverUrlNoticia: 'https://preapi-interno.www.gov.co/api/noticias/',
   urlApiEntidades: 'https://api-interno.www.gov.co/api/entidades',
-  apiUrl: "https://preapi-interno.www.gov.co/api/participacion/",
-  apiUrlUrna: "https://preapi-interno.www.gov.co/api/participacion/",
+  apiUrl: 'https://preapi-interno.www.gov.co/api/participacion/',
+  apiUrlUrna: 'https://preapi-interno.www.gov.co/api/participacion/',
 
-  apiCrossUrl: "https://preapi-interno.www.gov.co/api/cross/",
-  severApiCross: "https://preapi-interno.www.gov.co/api/cross/",
+  apiCrossUrl: 'https://preapi-interno.www.gov.co/api/cross/',
+  severApiCross: 'https://preapi-interno.www.gov.co/api/cross/',
   //Home
-  serverUrlHome: "https://preapi-interno.www.gov.co/api/home",
-  serverUrlHomeAdm: "https://preapi-interno.www.gov.co/api/home",
-  serverUrlHomeAdmP: "https://preapi-interno.www.gov.co/api/home",
+  serverUrlHome: 'https://preapi-interno.www.gov.co/api/home',
+  serverUrlHomeAdm: 'https://preapi-interno.www.gov.co/api/home',
+  serverUrlHomeAdmP: 'https://preapi-interno.www.gov.co/api/home',
   //Nosotros
-  serverUrlHomeSNosotros: "https://preapi-interno.www.gov.co/api/home",
+  serverUrlHomeSNosotros: 'https://preapi-interno.www.gov.co/api/home',
   //Transversales
   urlApiFooter: 'https://preapi-interno.www.gov.co/api/cross',
   //Noticias o Actualidad
-  apiNoticiasDetalle: "https://preapi-interno.www.gov.co/api/noticias/",
-  serverCategoriaSubcategoriaUrl: 'https://preapi-interno.www.gov.co/api/categorias-subcategorias/',
+  apiNoticiasDetalle: 'https://preapi-interno.www.gov.co/api/noticias/',
+  serverCategoriaSubcategoriaUrl:
+    'https://preapi-interno.www.gov.co/api/categorias-subcategorias/',
   codNoticias: 'actualidadprincipal',
-  codParticipa: 'tuopinioncuenta',
+  codParticipa: 'participacion',
   codSobreNosotros: 'sobrenosotros',
-  codEntidades: 'paraentidades',
+  codEntidades: 'servicios-para-entidades',
   //Entidades
-  serverIntegracion: 'https://preapi-interno.www.gov.co/api/integracion-sedes/IntegracionSedes/entidad/',
+  serverIntegracion:
+    'https://preapi-interno.www.gov.co/api/integracion-sedes/IntegracionSedes/entidad/',
   //Tu opinion cuenta o Participacion
-  apiCross: "https://preapi-interno.www.gov.co/api/cross/",
-  apiUtils: "https://preapi-interno.www.gov.co/api/utils/",
+  apiCross: 'https://preapi-interno.www.gov.co/api/cross/',
+  apiUtils: 'https://preapi-interno.www.gov.co/api/utils/',
   //Biblioteca
-  serverBiblioteca:"https://preapi-interno.www.gov.co/api/biblioteca/",
-  serverUrlWebApi:"https://preapi-interno.www.gov.co/api/biblioteca/",
+  serverBiblioteca: 'https://preapi-interno.www.gov.co/api/biblioteca/',
+  serverUrlWebApi: 'https://preapi-interno.www.gov.co/api/biblioteca/',
 
-  urlApiTramites: "https://preapi-interno.www.gov.co/api/ficha-tramites-y-servicios",
-  serverUrlNoticias: "https://preapi-interno.www.gov.co/api/noticias/",
+  urlApiTramites:
+    'https://preapi-interno.www.gov.co/api/ficha-tramites-y-servicios',
+  serverUrlNoticias: 'https://preapi-interno.www.gov.co/api/noticias/',
   //Categorias-Subcategorias o momentos de vida
-  serverCategorias: "https://preapi-interno.www.gov.co/api/categorias-subcategorias/",
+  serverCategorias:
+    'https://preapi-interno.www.gov.co/api/categorias-subcategorias/',
 
   //URL plan de integracion
-  urlPlanIntegracion: "https://preadmin.www.gov.co/Autenticacion/Autenticacion/RegistroAutoridad",
+  urlPlanIntegracion:
+    'https://preadmin.www.gov.co/Autenticacion/Autenticacion/RegistroAutoridad',
 
-  UrlAPIConsultaCIIU: 'https://preapi-interno.www.gov.co/api/ficha-tramites-y-servicios/ConsultaCIIU/',
+  UrlAPIConsultaCIIU:
+    'https://preapi-interno.www.gov.co/api/ficha-tramites-y-servicios/ConsultaCIIU/',
   auditoriaurl: 'https://preapi-interno.www.gov.co/api/auditoria/Auditoria/',
   serverValidateUrl: 'https://preapi-interno.www.gov.co/api/utils/validateurl',
 
   //Ficha tramite
-  serverUrlFichaTramite: 'https://preapi-interno.www.gov.co/api/ficha-tramites-y-servicios/',
+  serverUrlFichaTramite:
+    'https://preapi-interno.www.gov.co/api/ficha-tramites-y-servicios/',
   //ventanilla unica
-  serverSedesElectronicas: "https://preapi-interno.www.gov.co/api/integracion-sedes",
+  serverSedesElectronicas:
+    'https://preapi-interno.www.gov.co/api/integracion-sedes',
   //Auth
   authIssuer: 'https://preautenticaciondigital.and.gov.co',
-  clientID: "govcomovilPRE",
-  authWebApi:"https://preapi-interno.www.gov.co/api/roles-y-permisos/RolesPerfiles/Usuarios",
-  URLgovco : 'https://pwa-dev.and.gov.co/',
+  clientID: 'govcomovilPRE',
+  authWebApi:
+    'https://preapi-interno.www.gov.co/api/roles-y-permisos/RolesPerfiles/Usuarios',
+  URLgovco: 'https://pwa-dev.and.gov.co/',
   //WebComponent
-  serverFichaTramite:'https://preapi-interno.www.gov.co/api/ficha-tramites-y-servicios',
+  serverFichaTramite:
+    'https://preapi-interno.www.gov.co/api/ficha-tramites-y-servicios',
 
   //buscador
   serverBuscador: 'http://100.24.232.62:8083/',
   serverUrlValidarUrl: 'http://3.237.183.133:8184/api',
 
   //captcha
-  keyCaptcha: "6LeCqOcUAAAAAMu9McR1zoUH0z6wDj_dekAODW5h",
+  keyCaptcha: '6LeCqOcUAAAAAMu9McR1zoUH0z6wDj_dekAODW5h',
 
   //Carpeta ciudadana
-  carpetaciudadana:"https://precarpetaciudadana.and.gov.co/",
+  carpetaciudadana: 'https://precarpetaciudadana.and.gov.co/',
 };
