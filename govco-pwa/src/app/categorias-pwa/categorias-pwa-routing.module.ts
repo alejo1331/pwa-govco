89f013c08bf2dd5bf8d1f8227e3f6e24dd0edcf7
Jorge Pacheco <jorgepachecorodev@gmail.com>
2023-05-31 14:43:53 +0100

    #fix: Resolucion de conflictos

--- govco-pwa/src/app/categorias-pwa/categorias-pwa-routing.module.ts
+++ govco-pwa/src/app/categorias-pwa/categorias-pwa-routing.module.ts
@@ -4,11 +4,8 @@ import { MomentosDeVidaComponent } from './components/momentos-de-vida/momentos-
 import { DetalleMomentosDeVidaComponent } from './components/detalle-momentos-de-vida/detalle-momentos-de-vida.component';
 import { urlsLocal } from 'src/variables-globales/urlsLocal';
 import { LoMasConsultadoComponent } from './components/lo-mas-consultado/lo-mas-consultado.component';
-<<<<<<< HEAD
 import { TodosLosTramitesComponent } from './components/todos-los-tramites/todos-los-tramites.component';
-=======
 import { TramitesDestacadosComponent } from './components/tramites-destacados/tramites-destacados.component';
->>>>>>> 39ff8c91 (feat: #HU:4.1.1 #SP:1 #comment Maquetacion seccion de tramites destacados, enrrutamiento de seccion y boton de ver mas)
 
 const routes: Routes = [
   {
@@ -27,13 +24,12 @@ const routes: Routes = [
             component: LoMasConsultadoComponent
           },
           {
-<<<<<<< HEAD
             path: 'todos-los-tramites',
             component: TodosLosTramitesComponent
-=======
+          },
+          {
             path: 'tramites-destacados',
             component: TramitesDestacadosComponent
->>>>>>> 39ff8c91 (feat: #HU:4.1.1 #SP:1 #comment Maquetacion seccion de tramites destacados, enrrutamiento de seccion y boton de ver mas)
           },
         ]
       },
